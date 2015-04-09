463d87e88b0ce3eeba215f16e5d00b721a70c83d
Jonathan Storm <the.jonathan.storm@gmail.com>
2015-04-08 23:23:57 -0500

    Changes, changes...

--- lib/linear.ex
+++ lib/linear.ex
@@ -3,15 +3,6 @@
 # terms of the Do What The Fuck You Want To Public License, Version 2,
 # as published by Sam Hocevar. See the COPYING.WTFPL file for more details.
 
-defmodule Linear do
-  defmacro __using__(_opts) do
-    quote do
-      import Vector
-      import Matrix
-    end
-  end
-end
-
 defprotocol Vector do
   def add(vector1, vector2)
   def bit_and(vector1, vector2)
@@ -64,7 +55,7 @@ defimpl Vector, for: BitString do
 
     (for ui <- u do
       for vj <- v, do: ui * vj
-    end) |> :binary.list_to_bin
+    end) |> Enum.map(&(:binary.list_to_bin &1))
   end
 
   def subtract(bitstring1, bitstring2) do
@@ -87,64 +78,70 @@ end
 defimpl Vector, for: List do
   use Bitwise
 
-  defp vector_op(list1, list2, fun) when is_list(list1) and is_list(list2) do
+  defp vector_op(list1, list2, fun) do
     Enum.zip(list1, list2)
       |> Enum.map(fun)
   end
 
-  def add(list1, list2) when is_list(list1) and is_list(list2) do
+  def add(list1, list2) do
     vector_op(list1, list2, fn {ui, vi} -> ui + vi end)
   end
 
-  def bit_and(list1, list2) when is_list(list1) and is_list(list2) do
+  def bit_and(list1, list2) do
     vector_op(list1, list2, fn {ui, vi} -> band(ui, vi) end)
   end
 
-  def inner(list1, list2) when is_list(list1) and is_list(list2) do
+  def inner(list1, list2) do
     vector_op(list1, list2, fn {ui, vi} -> ui * vi end)
       |> Enum.sum
   end
 
-  def subtract(list1, list2) when is_list(list1) and is_list(list2) do
+  def subtract(list1, list2) do
     vector_op(list1, list2, fn {ui, vi} -> ui - vi end)
   end
 
-  def bit_xor(list1, list2) when is_list(list1) and is_list(list2) do
+  def bit_xor(list1, list2) do
     vector_op(list1, list2, fn {ui, vi} -> bxor(ui, vi) end)
   end
 
-  def outer(list1, list2) when is_list(list1) and is_list(list2) do
+  def outer(list1, list2) do
     [u, v] = [list1, list2]
 
     (for ui <- u do
       for vj <- v, do: ui * vj
-    end) |> List.flatten
+    end)
   end
 end
 
-defmodule Matrix do
-  def dimension(matrix) when is_list(matrix) do
-    [row|_tail] = matrix
+defprotocol Matrix do
+  def dimension(matrix)
+  def multiply(matrix, matrix_or_vector)
+  def transpose(matrix_or_vector)
+end
 
-    {length(matrix), length(row)}
+defimpl Matrix, for: List do
+  def dimension([row|tail]) when is_list(row) do
+    {length([row|tail]), length(row)}
   end
 
-  def multiply([row|_t], [row2|_t2]) when is_list(row) and is_list(row2) do
-    for row <- [row|_t] do
-      for col <- transpose([row2|_t2]) do
+  def multiply([row|tail], [row2|tail2]) when is_list(row) and is_list(row2) do
+    for row <- [row|tail] do
+      for col <- transpose [row2|tail2] do
         Vector.inner(row, col)
       end
     end
   end
-  def multiply([row|_tail], vector) when is_list(row) and is_list(vector) do
-    multiply([row|_tail], transpose([vector]))
+  def multiply([row|tail], vector) when is_list(row) and is_list(vector) do
+    multiply([row|tail], transpose [vector])
   end
 
-  def transpose([row|_tail]) when is_list(row) do
-    [row|_tail]
+  def transpose([row|tail]) when is_list(row) do
+    [row|tail]
       |> List.zip
       |> Enum.map(&(Tuple.to_list &1))
   end
-  def transpose(vector) when is_list(vector), do: transpose([vector])
+  def transpose(vector) do
+    transpose [vector]
+  end
 end
 
--- test/linear_test.exs
+++ test/linear_test.exs
@@ -1,7 +1,56 @@
-defmodule LinearExTest do
-  use ExUnit.Case
+# Copyright © 2015 Jonathan Storm <the.jonathan.storm@gmail.com>
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See the COPYING.WTFPL file for more details.
 
-  test "the truth" do
-    assert 1 + 1 == 2
+defmodule LinearTest do
+  use ExUnit.Case, async: true
+
+  test "Vector.add returns correct result given two binaries" do
+    assert Vector.add(<<1, 2, 3>>, <<1, 2, 3>>) == <<2, 4, 6>>
+  end
+  test "Vector.bit_and returns correct result given two binaries" do
+    assert Vector.bit_and(<<1, 1, 1>>, <<3, 3, 3>>) == <<1, 1, 1>>
+  end
+  test "Vector.inner returns correct result given two binaries" do
+    assert Vector.inner(<<1, 2, 3>>, <<1, 2, 3>>) == 14
+  end
+  test "Vector.outer returns correct result given two binaries" do
+    assert Vector.outer(<<1, 2, 3>>, <<1, 2, 3>>) ==
+      [
+        <<1, 2, 3>>,
+        <<2, 4, 6>>,
+        <<3, 6, 9>>
+      ]
+  end
+  test "Vector.subtract returns correct result given two binaries" do
+    assert Vector.subtract(<<1, 2, 3>>, <<1, 2, 3>>) == <<0, 0, 0>>
+  end
+  test "Vector.bit_xor returns correct result given two binaries" do
+    assert Vector.bit_xor(<<1, 1, 1>>, <<3, 3, 3>>) == <<2, 2, 2>>
+  end
+
+  test "Vector.add returns correct result given two lists" do
+    assert Vector.add([1, 1, 1], [2, 2, 2]) == [3, 3, 3]
+  end
+  test "Vector.bit_and returns correct result given two lists" do
+    assert Vector.bit_and([1, 1, 1], [3, 3, 3]) == [1, 1, 1]
+  end
+  test "Vector.inner returns correct result given two lists" do
+    assert Vector.inner([1, 1, 1], [2, 2, 2]) == 6
+  end
+  test "Vector.outer returns correct result given two lists" do
+    assert Vector.outer([1, 2, 3], [1, 2, 3]) ==
+      [
+        [1, 2, 3],
+        [2, 4, 6],
+        [3, 6, 9]
+      ]
+  end
+  test "Vector.subtract returns correct result given two lists" do
+    assert Vector.subtract([1, 2, 3], [1, 2, 3]) == [0, 0, 0]
+  end
+  test "Vector.bit_xor returns correct result given two lists" do
+    assert Vector.bit_xor([1, 1, 1], [3, 3, 3]) == [2, 2, 2]
   end
 end
