73644afc7f6e769b1f58c2ea02465b951357de52
Jonathan D. Storm <jonathan.dale.storm@protonmail.ch>
2015-08-01 15:37:57 -0500

    Add multiply, divide, absolute to Vector protocol; some refactoring

--- lib/linear.ex
+++ lib/linear.ex
@@ -6,13 +6,104 @@
 defprotocol Vector do
   def add(vector1, vector2)
   def subtract(vector1, vector2)
+  def multiply(vector1, vector2)
+  def divide(vector1, vector2)
   def inner(vector1, vector2)
   def outer(vector1, vector2)
   def bit_and(vector1, vector2)
   def bit_xor(vector1, vector2)
+  def absolute(vector)
   def mod(vector, modulus)
   def dimension(vector)
   def embed(vector, dimension)
+end
+
+defimpl Vector, for: List do
+  use Bitwise
+
+  defp vector_op(list1, list2, fun) when length(list1) == length(list2) do
+    Enum.zip(list1, list2) |> Enum.map(fun)
+  end
+  defp vector_op(_, _, _) do
+    raise ArgumentError, message: "Vectors must be of same dimension"
+  end
+
+  def add(u, v) when is_list v do
+    vector_op u, v, fn {ui, vi} -> ui + vi end
+  end
+  def add(v, scalar) when is_number scalar do
+    Enum.map v, fn vi -> vi + scalar end
+  end
+
+  def multiply(u, v) when is_list v do
+    vector_op u, v, fn {ui, vi} -> ui * vi end
+  end
+  def multiply(v, scalar) when is_number scalar do
+    Enum.map v, fn vi -> vi * scalar end
+  end
+
+  def subtract(u, v) when is_list v do
+    add u, multiply(v, -1)
+  end
+  def subtract(v, scalar) when is_number scalar do
+    Enum.map v, fn vi -> vi - scalar end
+  end
+
+  def divide(u, v) when is_list v do
+    vector_op(u, v, fn {ui, vi} -> ui / vi end)
+  end
+  def divide(v, scalar) when is_number scalar do
+    Enum.map v, fn vi -> vi / scalar end
+  end
+
+  def inner(u, v) when is_list v do
+    multiply(u, v) |> Enum.sum
+  end
+
+  def outer(u, v) when is_list v do
+    for ui <- u do
+      multiply(v, ui)
+    end
+  end
+
+  def bit_and(u, v) when is_list v do
+    vector_op u, v, fn {ui, vi} -> band(ui, vi) end
+  end
+  def bit_and(v, scalar) when is_number scalar do
+    Enum.map v, fn vi -> band(vi, scalar) end
+  end
+
+  def bit_xor(u, v) when is_list v do
+    vector_op u, v, fn {ui, vi} -> bxor(ui, vi) end
+  end
+  def bit_xor(v, scalar) when is_number scalar do
+    Enum.map v, fn vi -> bxor(vi, scalar) end
+  end
+
+  def absolute(v) do
+    Enum.map v, fn vi -> abs vi end
+  end
+
+  def mod(v, modulus) do
+    Enum.map v, fn vi -> Math.mod(vi, modulus) end
+  end
+
+  def embed(v, dimension) when length(v) == dimension do
+    v
+  end
+  def embed(v, dimension) when length(v) < dimension do
+    v
+    |> :binary.list_to_bin
+    |> String.rjust(dimension, 0)
+    |> :binary.bin_to_list
+  end
+  def embed(list, dimension) when length(list) > dimension do
+    raise ArgumentError, message: "Cannot embed vector in space of lower dimension"
+  end
+
+  def dimension(v) do
+    length v
+  end
 end
 
 defimpl Vector, for: BitString do
@@ -32,118 +123,111 @@ defimpl Vector, for: BitString do
     raise ArgumentError, message: "Vectors must be of same dimension"
   end
 
-  def add(u, v) do
-    vector_op(u, v, fn {ui, vi} -> ui + vi end)
+  def add(u, v) when is_binary v do
+    vector_op(u, v, fn {ui, vi} -> Math.mod(ui + vi, 256) end)
+  end
+  def add(v, scalar) when is_integer scalar do
+    v
+    |> :binary.bin_to_list
+    |> Enum.map(fn vi -> Math.mod(vi + scalar, 256) end)
+    |> :binary.list_to_bin
   end
 
-  def subtract(u, v) do
-    vector_op(u, v, fn {ui, vi} -> Math.mod(ui - vi, 256) end)
+  def multiply(u, v) when is_binary v do
+    vector_op(u, v, fn {ui, vi} -> Math.mod(ui * vi, 256) end)
+  end
+  def multiply(v, scalar) when is_integer scalar do
+    v
+    |> :binary.bin_to_list
+    |> Enum.map(fn vi -> Math.mod(vi * scalar, 256) end)
+    |> :binary.list_to_bin
   end
 
-  def inner(u, v) do
-    vector_op(u, v, fn {ui, vi} -> ui * vi end)
+  def subtract(u, v) when is_binary v do
+    add u, multiply(v, -1)
+  end
+  def subtract(v, scalar) when is_integer scalar do
+    v
+    |> :binary.bin_to_list
+    |> Enum.map(fn vi -> Math.mod(vi - scalar, 256) end)
+    |> :binary.list_to_bin
+  end
+
+  def divide(u, v) when is_binary v do
+    vector_op(u, v, fn {ui, vi} -> div(ui, vi) |> Math.mod(256) end)
+  end
+  def divide(v, scalar) when is_integer scalar do
+    v
+    |> :binary.bin_to_list
+    |> Enum.map(fn vi -> div(vi, scalar) |> Math.mod(256) end)
+    |> :binary.list_to_bin
+  end
+
+  def inner(u, v) when is_binary v do
+    u
+    |> multiply(v)
     |> :binary.bin_to_list
     |> Enum.sum
   end
 
-  def outer(bitstring1, bitstring2) do
+  def outer(bitstring1, bitstring2) when is_binary bitstring2 do
     [u, v] = bitstrings_to_lists [bitstring1, bitstring2]
 
     for ui <- u do
-      for vj <- v, do: ui * vj
+      for vj <- v, do: Math.mod(ui * vj, 256)
     end
-    |> Enum.map(&(:binary.list_to_bin &1))
+    |> Enum.map(&:binary.list_to_bin(&1))
   end
-  
-  def bit_and(u, v) do
+
+  def bit_and(u, v) when is_binary v do
     vector_op(u, v, fn {ui, vi} -> band(ui, vi) end)
   end
-
-  def bit_xor(u, v) do
-    vector_op(u, v, fn {ui, vi} -> bxor(ui, vi) end)
+  def bit_and(v, scalar) when is_integer scalar do
+    v
+    |> :binary.bin_to_list
+    |> Enum.map(fn vi -> scalar |> Math.mod(256) |> band(vi) end)
+    |> :binary.list_to_bin
   end
 
-  def mod(bitstring, modulus) do
-    bitstring
+  def bit_xor(u, v) when is_binary v do
+    vector_op(u, v, fn {ui, vi} -> bxor(ui, vi) end)
+  end
+  def bit_xor(u, v) when is_binary v do
+    vector_op(u, v, fn {ui, vi} -> band(ui, vi) end)
+  end
+  def bit_xor(v, scalar) when is_integer scalar do
+    v
+    |> :binary.bin_to_list
+    |> Enum.map(fn vi -> scalar |> Math.mod(256) |> bxor(vi) end)
+    |> :binary.list_to_bin
+  end
+
+  def absolute(v) do
+    v
+  end
+
+  def mod(v, modulus) do
+    v
     |> :binary.bin_to_list
     |> Enum.map(fn vi -> Math.mod(vi, modulus) end)
     |> :binary.list_to_bin
   end
 
-  def dimension(bitstring) do
-    byte_size bitstring
+  def dimension(v) do
+    byte_size v
   end
 
-  def embed(bitstring, dimension) when byte_size(bitstring) == dimension do
-    bitstring
+  def embed(v, dimension) when byte_size(v) == dimension do
+    v
   end
-  def embed(bitstring, dimension) when byte_size(bitstring) < dimension do
-    String.rjust(bitstring, dimension, 0)
+  def embed(v, dimension) when byte_size(v) < dimension do
+    String.rjust(v, dimension, 0)
   end
-  def embed(bitstring, dimension) when byte_size(bitstring) > dimension do
+  def embed(v, dimension) when byte_size(v) > dimension do
     raise ArgumentError, message: "Cannot embed vector in space of lower dimension"
   end
 end
 
-defimpl Vector, for: List do
-  use Bitwise
-
-  defp vector_op(list1, list2, fun) when length(list1) == length(list2) do
-    Enum.zip(list1, list2) |> Enum.map(fun)
-  end
-  defp vector_op(_, _, _) do
-    raise ArgumentError, message: "Vectors must be of same dimension"
-  end
-
-  def add(u, v) do
-    vector_op(u, v, fn {ui, vi} -> ui + vi end)
-  end
-
-  def subtract(u, v) do
-    vector_op(u, v, fn {ui, vi} -> ui - vi end)
-  end
-
-  def inner(u, v) do
-    vector_op(u, v, fn {ui, vi} -> ui * vi end) |> Enum.sum
-  end
-
-  def outer(list1, list2) do
-    [u, v] = [list1, list2]
-
-    for ui <- u do
-      for vj <- v, do: ui * vj
-    end
-  end
-
-  def bit_and(u, v) do
-    vector_op(u, v, fn {ui, vi} -> band(ui, vi) end)
-  end
-
-  def bit_xor(u, v) do
-    vector_op(u, v, fn {ui, vi} -> bxor(ui, vi) end)
-  end
-
-  def mod(list, modulus) do
-    Enum.map(list, fn vi -> Math.mod(vi, modulus) end)
-  end
-
-  def embed(list, dimension) when length(list) == dimension do
-    list
-  end
-  def embed(list, dimension) when length(list) < dimension do
-    list
-    |> :binary.list_to_bin
-    |> String.rjust(dimension, 0)
-    |> :binary.bin_to_list
-  end
-  def embed(list, dimension) when length(list) > dimension do
-    raise ArgumentError, message: "Cannot embed vector in space of lower dimension"
-  end
-
-  def dimension(list) do
-    length list
-  end
-end
 
 defprotocol Matrix do
   def dimension(matrix)
@@ -152,7 +236,7 @@ defprotocol Matrix do
 end
 
 defimpl Matrix, for: List do
-  def dimension([row|tail]) when is_list(row) do
+  def dimension([row|tail]) when is_list row do
     {length([row|tail]), length(row)}
   end
 
@@ -167,7 +251,7 @@ defimpl Matrix, for: List do
     multiply([row|tail], transpose [vector])
   end
 
-  def transpose([row|tail]) when is_list(row) do
+  def transpose([row|tail]) when is_list row do
     [row|tail]
     |> List.zip
     |> Enum.map(&(Tuple.to_list &1))
--- test/linear_test.exs
+++ test/linear_test.exs
@@ -3,65 +3,63 @@
 # terms of the Do What The Fuck You Want To Public License, Version 2,
 # as published by Sam Hocevar. See the COPYING.WTFPL file for more details.
 
+
+## List ##
+
 defmodule LinearTest do
   use ExUnit.Case, async: true
 
-  test "adds two binaries" do
-    assert Vector.add(<<1, 2, 3>>, <<1, 2, 3>>) == <<2, 4, 6>>
-  end
-
-  test "subtracts two binaries" do
-    assert Vector.subtract(<<1, 2, 3>>, <<1, 2, 3>>) == <<0, 0, 0>>
-  end
-  test "subtracts two binaries resulting in negative components" do
-    assert Vector.subtract(<<1, 2, 3>>, <<2, 2, 3>>) == <<255, 0, 0>>
-  end
-
-  test "dots two binaries" do
-    assert Vector.inner(<<1, 2, 3>>, <<1, 2, 3>>) == 14
-  end
-
-  test "takes the outer product of two binaries" do
-    assert Vector.outer(<<1, 2, 3>>, <<1, 2, 3, 4>>) ==
-      [
-        <<1, 2, 3,  4>>,
-        <<2, 4, 6,  8>>,
-        <<3, 6, 9, 12>>
-      ]
-  end
-
-  test "ANDs two binaries" do
-    assert Vector.bit_and(<<1, 1, 1>>, <<3, 3, 3>>) == <<1, 1, 1>>
-  end
-
-  test "XORs two binaries" do
-    assert Vector.bit_xor(<<1, 1, 1>>, <<3, 3, 3>>) == <<2, 2, 2>>
-  end
-
-  test "takes the modulo of each component of a binary" do
-    assert Vector.mod(<<2, 3, 4>>, 2) == <<0, 1, 0>>
-  end
-
-  test "embeds binary vector in space of greater dimension" do
-    assert Vector.embed(<<1, 2, 3>>, 5) == <<0, 0, 1, 2, 3>>
-  end
-
   test "adds two lists" do
     assert Vector.add([1, 1, 1], [2, 2, 2]) == [3, 3, 3]
+  end
+  test "adding lists of unequal length raises an error" do
+    assert_raise ArgumentError, fn -> Vector.add([1, 2], [1, 2, 3]) end
+  end
+  test "adds a scalar to a list" do
+    assert Vector.add([0, 0, 0], 1) == [1, 1, 1]
+  end
+
+  test "multiplies two lists" do
+    assert Vector.multiply([1, 2, 3], [1, 2, 3]) == [1, 4, 9]
+  end
+  test "multiplies two lists without component wrap" do
+    assert Vector.multiply([128, 128, 128], [2, 4, 6]) == [256, 512, 768]
+  end
+  test "multiplying lists of unequal length raises an error" do
+    assert_raise ArgumentError, fn -> Vector.multiply([1, 2], [1, 2, 3]) end
+  end
+  test "multiplies a list by a scalar" do
+    assert Vector.multiply([1, 2, 3], 3) == [3, 6, 9]
   end
 
   test "subtracts two lists" do
     assert Vector.subtract([1, 2, 3], [1, 2, 3]) == [0, 0, 0]
   end
-  test "subtracts two lists resulting in negative components" do
+  test "subtracts two lists without component wrap" do
     assert Vector.subtract([1, 2, 3], [2, 2, 3]) == [-1, 0, 0]
   end
-  test "lists of different length raise an error" do
-    assert_raise ArgumentError, fn -> Vector.add([1, 2], [1, 2, 3]) end
+  test "subtracting lists of unequal length raises an error" do
+    assert_raise ArgumentError, fn -> Vector.subtract([1, 2], [1, 2, 3]) end
+  end
+  test "subtracts a scalar from a list" do
+    assert Vector.subtract([0, 0, 0], 1) == [-1, -1, -1]
+  end
+
+  test "divides two lists" do
+    assert Vector.divide([3, 3, 3], [1, 2, 3]) == [3, 1.5, 1]
+  end
+  test "dividing lists of unequal length raises an error" do
+    assert_raise ArgumentError, fn -> Vector.divide([1, 2], [1, 2, 3]) end
+  end
+  test "divides a list by a scalar" do
+    assert Vector.divide([3, 3, 3], 2) == [1.5, 1.5, 1.5]
   end
 
   test "dots two lists" do
     assert Vector.inner([1, 1, 1], [2, 2, 2]) == 6
+  end
+  test "dot-ing lists of unequal length raises an error" do
+    assert_raise ArgumentError, fn -> Vector.inner([1, 2], [1, 2, 3]) end
   end
 
   test "takes the outer product of two lists" do
@@ -76,16 +74,160 @@ defmodule LinearTest do
   test "ANDs two lists" do
     assert Vector.bit_and([1, 1, 1], [3, 3, 3]) == [1, 1, 1]
   end
+  test "ANDing lists of unequal length raises an error" do
+    assert_raise ArgumentError, fn -> Vector.bit_and([1, 2], [1, 2, 3]) end
+  end
+  test "ANDs a scalar with a list" do
+    assert Vector.bit_and([3, 3, 3], 1) == [1, 1, 1]
+  end
 
   test "XORs two lists" do
     assert Vector.bit_xor([1, 1, 1], [3, 3, 3]) == [2, 2, 2]
   end
+  test "XORing lists of unequal length raises an error" do
+    assert_raise ArgumentError, fn -> Vector.bit_xor([1, 2], [1, 2, 3]) end
+  end
+  test "XORs a scalar with a list" do
+    assert Vector.bit_xor([3, 3, 3], 1) == [2, 2, 2]
+  end
 
-  test "takes the modulo of each component of a list" do
+  test "takes the absolute value of each component in a list" do
+    assert Vector.absolute([-1, -2, -3]) == [1, 2, 3]
+  end
+
+  test "takes the modulo of each component in a list" do
     assert Vector.mod([2, 3, 4], 2) == [0, 1, 0]
   end
 
   test "embeds list vector in space of greater dimension" do
     assert Vector.embed([1, 2, 3], 5) == [0, 0, 1, 2, 3]
+  end
+
+
+  ## Bitstring ##
+
+  test "adds two bitstrings" do
+    assert Vector.add(<<1, 2, 3>>, <<1, 2, 3>>) == <<2, 4, 6>>
+  end
+  test "adds two bitstrings, resulting in component wrap" do
+    assert Vector.add(<<0, 0, 255>>, <<0, 0, 1>>) == <<0, 0, 0>>
+  end
+  test "adding bitstrings of unequal length raises an error" do
+    assert_raise ArgumentError, fn -> Vector.add(<<1, 2>>, <<1, 2, 3>>) end
+  end
+  test "adds a scalar to a bitstring" do
+    assert Vector.add(<<0, 0, 0>>, 1) == <<1, 1, 1>>
+  end
+  test "adds a scalar to a bitstring, resulting in component wrap" do
+    assert Vector.add(<<255, 255, 255>>, 1) == <<0, 0, 0>>
+  end
+  test "adds a negative scalar to a bitstring" do
+    assert Vector.add(<<0, 0, 0>>, -1) == <<255, 255, 255>>
+  end
+
+  test "multiplies two bitstrings" do
+    assert Vector.multiply(<<1, 2, 3>>, <<1, 2, 3>>) == <<1, 4, 9>>
+  end
+  test "multiplies two bitstrings, resulting in component wrap" do
+    assert Vector.multiply(<<128, 128, 128>>, <<2, 4, 6>>) == <<0, 0, 0>>
+  end
+  test "multiplying bitstrings of unequal length raises an error" do
+    assert_raise ArgumentError, fn -> Vector.multiply(<<1, 2>>, <<1, 2, 3>>) end
+  end
+  test "multiplies a bitstring by a scalar" do
+    assert Vector.multiply(<<1, 2, 3>>, 3) == <<3, 6, 9>>
+  end
+  test "multiplies a bitstring by a negative scalar" do
+    assert Vector.multiply(<<1, 2, 3>>, -1) == <<255, 254, 253>>
+  end
+
+  test "subtracts two bitstrings" do
+    assert Vector.subtract(<<1, 2, 3>>, <<1, 2, 3>>) == <<0, 0, 0>>
+  end
+  test "subtracts two bitstrings, resulting in component wrap" do
+    assert Vector.subtract(<<1, 2, 3>>, <<2, 2, 3>>) == <<255, 0, 0>>
+  end
+  test "subtracting bitstrings of unequal length raises an error" do
+    assert_raise ArgumentError, fn -> Vector.subtract(<<1, 2>>, <<1, 2, 3>>) end
+  end
+  test "subtracts a scalar from a binary" do
+    assert Vector.subtract(<<0, 0, 0>>, 1) == <<255, 255, 255>>
+  end
+  test "subtracts a negative scalar from a binary" do
+    assert Vector.subtract(<<0, 0, 0>>, -1) == <<1, 1, 1>>
+  end
+
+  test "divides two bitstrings" do
+    assert Vector.divide(<<3, 3, 3>>, <<1, 2, 3>>) == <<3, 1, 1>>
+  end
+  test "dividing bitstrings of unequal length raises an error" do
+    assert_raise ArgumentError, fn -> Vector.divide(<<1, 2>>, <<1, 2, 3>>) end
+  end
+  test "divides a bitstring by a scalar" do
+    assert Vector.divide(<<0, 1, 2>>, 2) == <<0, 0, 1>>
+  end
+  test "divides a bitstring by a negative scalar" do
+    assert Vector.divide(<<255, 255, 255>>, -1) == <<1, 1, 1>>
+  end
+
+  test "dots two bitstrings" do
+    assert Vector.inner(<<1, 2, 3>>, <<1, 2, 3>>) == 14
+  end
+  test "dots two bitstrings, resulting in component wrap" do
+    assert Vector.inner(<<128, 128, 128>>, <<2, 2, 2>>) == 0
+  end
+  test "dot-ing bitstrings of unequal length raises an error" do
+    assert_raise ArgumentError, fn -> Vector.inner(<<1, 2>>, <<1, 2, 3>>) end
+  end
+
+  test "takes the outer product of two bitstrings" do
+    assert Vector.outer(<<1, 2, 3>>, <<1, 2, 3, 4>>) ==
+      [
+        <<1, 2, 3,  4>>,
+        <<2, 4, 6,  8>>,
+        <<3, 6, 9, 12>>
+      ]
+  end
+  test "takes the outer product of two bitstrings, resulting in component wrap" do
+    assert Vector.outer(<<128, 128, 128>>, <<2, 4, 6, 8>>) ==
+      [
+        <<0, 0, 0, 0>>,
+        <<0, 0, 0, 0>>,
+        <<0, 0, 0, 0>>
+      ]
+  end
+
+  test "ANDs two bitstrings" do
+    assert Vector.bit_and(<<1, 1, 1>>, <<3, 3, 3>>) == <<1, 1, 1>>
+  end
+  test "ANDing bitstrings of unequal length raises an error" do
+    assert_raise ArgumentError, fn -> Vector.bit_and(<<1, 2>>, <<1, 2, 3>>) end
+  end
+  test "ANDs a scalar with a bitstring" do
+    assert Vector.bit_and(<<1, 1, 1>>, 3) == <<1, 1, 1>>
+  end
+  test "ANDs a negative scalar with a bitstring" do
+    assert Vector.bit_and(<<255, 255, 255>>, -255) == <<1, 1, 1>>
+  end
+
+  test "XORs two bitstrings" do
+    assert Vector.bit_xor(<<1, 1, 1>>, <<3, 3, 3>>) == <<2, 2, 2>>
+  end
+  test "XORing bitstrings of unequal length raises an error" do
+    assert_raise ArgumentError, fn -> Vector.bit_xor(<<1, 2>>, <<1, 2, 3>>) end
+  end
+  test "XORs a scalar with a bitstring" do
+    assert Vector.bit_xor(<<1, 1, 1>>, 3) == <<2, 2, 2>>
+  end
+  test "XORs a negative scalar with a bitstring" do
+    assert Vector.bit_xor(<<3, 3, 3>>, -255) == <<2, 2, 2>>
+  end
+
+  test "takes the modulo of each component of a bitstring" do
+    assert Vector.mod(<<2, 3, 4>>, 2) == <<0, 1, 0>>
+  end
+
+  test "embeds bitstring vector in space of greater dimension" do
+    assert Vector.embed(<<1, 2, 3>>, 5) == <<0, 0, 1, 2, 3>>
   end
 end
