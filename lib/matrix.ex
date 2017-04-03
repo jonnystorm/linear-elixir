a755ad11bcd13ad0d54ddfdadfa00a73b1f2337f
Jonathan Storm <jonnystorm@users.noreply.github.com>
2017-04-03 00:40:00 -0500

    Change license to MPLv2; reformatting (#1)

--- lib/matrix.ex
+++ lib/matrix.ex
@@ -1,7 +1,6 @@
-# Copyright © 2015 Jonathan Storm <the.jonathan.storm@gmail.com>
-# This work is free. You can redistribute it and/or modify it under the
-# terms of the Do What The Fuck You Want To Public License, Version 2,
-# as published by Sam Hocevar. See the COPYING.WTFPL file for more details.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 defprotocol Matrix do
   def dimension(object)
@@ -12,56 +11,55 @@ defprotocol Matrix do
 end
 
 defimpl Matrix, for: List do
-  def dimension(object = [row|_]) when is_list row do
-    {length(object), length(row)}
-  end
+  def dimension(object = [row|_]) when is_list(row),
+    do: {length(object), length(row)}
 
   def add(object1 = [row1|_], object2 = [row2|_])
-      when is_list(row1) and is_list(row2) do
-
+      when is_list(row1) and is_list(row2)
+  do
     object1
-      |> Enum.zip(object2)
-      |> Enum.map(fn {row1, row2} ->
-        Vector.add row1, row2
-      end)
+    |> Enum.zip(object2)
+    |> Enum.map(fn {row1, row2} ->
+      Vector.add(row1, row2)
+    end)
   end
 
   def subtract(object1 = [row1|_], object2 = [row2|_])
-      when is_list(row1) and is_list(row2) do
-
+      when is_list(row1) and is_list(row2)
+  do
     object2
-      |> multiply(-1)
-      |> add(object1)
+    |> multiply(-1)
+    |> add(object1)
   end
 
   def multiply(object1 = [row|_], object2 = [row2|_])
-      when is_list(row) and is_list(row2) do
-
+      when is_list(row) and is_list(row2)
+  do
     for row <- object1 do
-      for col <- transpose object2 do
-        Vector.inner row, col
+      for col <- transpose(object2) do
+        Vector.inner(row, col)
       end
     end
   end
+
   def multiply(object = [row|_], row_vector)
-      when is_list(row) and is_list(row_vector) do
+      when is_list(row) and is_list(row_vector),
+    do: multiply(object, transpose([row_vector]))
 
-    multiply object, transpose([row_vector])
-  end
   def multiply(object = [row|_], scalar)
-      when is_list(row) and is_number(scalar) do
-
+      when is_list(row) and is_number(scalar)
+  do
     for row <- object do
-      Vector.multiply row, scalar
+      Vector.multiply(row, scalar)
     end
   end
 
   def transpose(object = [row|_]) when is_list row do
     object
-      |> List.zip
-      |> Enum.map(&(Tuple.to_list &1))
+    |> List.zip
+    |> Enum.map(&(Tuple.to_list &1))
   end
-  def transpose(row_vector) do
-    transpose [row_vector]
-  end
+
+  def transpose(row_vector),
+    do: transpose [row_vector]
 end
--- mix.exs
+++ mix.exs
@@ -1,22 +1,42 @@
-defmodule LinearEx.Mixfile do
+defmodule Linear.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :linear_ex,
-     version: "0.0.3",
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [ app: :linear_ex,
+      version: "0.0.4",
+      name: "LinearEx",
+      source_url: "https://github.com/jonnystorm/linear-elixir",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      docs: [extras: ["README.md"]],
+      dialyzer: [
+        add_plt_apps: [
+          :logger,
+          :jds_math_ex,
+        ],
+        ignore_warnings: "dialyzer.ignore",
+        flags: [
+          :unmatched_returns,
+          :error_handling,
+          :race_conditions,
+          :underspecs,
+        ],
+      ],
+    ]
   end
 
   def application do
-    [applications: [:logger, :jds_math_ex]]
+    [ applications: [
+        :logger,
+        :jds_math_ex
+      ]
+    ]
   end
 
   defp deps do
-    [
-      {:jds_math_ex, git: "https://github.com/jonnystorm/jds-math-elixir"}
+    [ {:jds_math_ex, git: "https://github.com/jonnystorm/jds-math-elixir"},
     ]
   end
 end
